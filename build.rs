985707c225c8e3e3025715542a3371b55d83594e
Romain Goyet <romain.goyet@numworks.com>
2021-06-14 17:46:58 -0400

    Report errors in build.rs

--- build.rs
+++ build.rs
@@ -2,11 +2,14 @@ use cc::Build;
 use std::process::Command;
 
 fn main() {
-    Build::new().file("eadk/eadk.s").compile("asm");
+    // Assemble eadk.s
     println!("cargo:rerun-if-changed=eadk/eadk.s");
-    Command::new("python3")
-        .args(&["eadk/inliner.py", "src/icon.png", "target/icon.ld"])
-        .output()
-        .expect("failed to process icon");
+    Build::new().file("eadk/eadk.s").compile("asm");
+
+    // Turn icon.png into a linker-embeddable file
     println!("cargo:rerun-if-changed=src/icon.png");
+    let output = Command::new("./eadk/inliner.py")
+        .args(&["src/icon.png", "target/icon.ld"])
+        .output().expect("Failure to launch process");
+    assert!(output.status.success(), "{}", String::from_utf8_lossy(&output.stderr));
 }
--- eadk/inliner.py
+++ eadk/inliner.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import lz4.block
 import os.path
