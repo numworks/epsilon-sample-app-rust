28809e045d2d05066a0ce3d39313baeb4d57a24a
Adrien Degliame <adidf-web@laposte.net>
2021-12-26 22:09:51 +0100

    Add basic new function for Point struct

--- src/eadk.rs
+++ src/eadk.rs
@@ -66,6 +66,15 @@ pub struct Point {
     pub y: u16,
 }
 
+impl Point {
+    pub fn new(x: u16, y: u16) -> Self {
+        Self {
+            x,
+            y
+        }
+    }
+}
+
 pub mod backlight {
     pub fn set_brightness(brightness: u8) {
         unsafe {
