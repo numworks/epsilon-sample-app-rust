195456b61bc5a871990ba05cad724bf2c253b95f
Emilie Feral <emilie.feral@numworks.com>
2022-01-21 12:39:03 +0100

    [eadk] Uniformize with epsilon-sample-app-ccp

--- eadk/run.py
+++ eadk/run.py
@@ -5,36 +5,35 @@ import re
 import subprocess
 import sys
 
-def parse_slot_info(slot_info_file):
-  data = open(slot_info_file, "rb").read()
-  slot_info_pattern = re.compile(b'\xba\xdb\xee\xef(.{4})(.{4})\xba\xdb\xee\xef', flags=re.MULTILINE)
-  result = slot_info_pattern.search(data)
-  kernel_header_address = int.from_bytes(result.group(1), "little")
-  userland_header_address = int.from_bytes(result.group(2), "little")
-  return kernel_header_address, userland_header_address
+def parse_device_information(device_information_file):
+  data = open(device_information_file, "r").read()
+  pattern = re.compile(
+          "EXTERNAL_APPS_FLASH_START = (0[xX][0-9a-fA-F]{8});\n"
+          "EXTERNAL_APPS_FLASH_END = (0[xX][0-9a-fA-F]{8});\n"
+          "EXTERNAL_APPS_RAM_START = (0[xX][0-9a-fA-F]{8});\n"
+          "EXTERNAL_APPS_RAM_END = (0[xX][0-9a-fA-F]{8});\n",
+    flags=re.MULTILINE)
+  result = pattern.search(data)
+  external_apps_flash_start =int(result.group(1), base = 16)
+  external_apps_flash_end = int(result.group(2), base = 16)
+  external_apps_ram_start = int(result.group(3), base = 16)
+  external_apps_ram_end = int(result.group(4), base = 16)
 
-def parse_userland_header(userland_header_file):
-  data = open(userland_header_file, "rb").read()
-  header_pattern = re.compile(b'\xfe\xed\xc0\xde.{8}.{4}.{4}(.{4})(.{4})(.{4})(.{4})\xfe\xed\xc0\xde', flags=re.MULTILINE)
-  result = header_pattern.search(data)
-  external_apps_start_address = int.from_bytes(result.group(1), "little")
-  external_apps_end_address = int.from_bytes(result.group(2), "little")
-  return external_apps_start_address, external_apps_end_address
+  print(external_apps_flash_start)
 
-def load_elf(elf_file, app_index = 0):
+  return external_apps_flash_start, external_apps_flash_end, external_apps_ram_start, external_apps_ram_end
+
+def load_elf(elf_file, device_information, app_index):
+  # Generate bin file
   bin_file = os.path.splitext(elf_file)[0] + ".bin"
   subprocess.check_output(["arm-none-eabi-objcopy", "-O", "binary", elf_file, bin_file])
-  ram_address = 0x20000000
-  subprocess.check_output(["dfu-util", "-a", "0", "-s", str(hex(ram_address)) + ":16:force", "-U", "slot_info.bin"])
-  _,userland_header_address = parse_slot_info("slot_info.bin")
-  subprocess.check_output(["rm", "slot_info.bin"])
-  subprocess.check_output(["dfu-util", "-a", "0", "-s", str(hex(userland_header_address)) + ":40:force", "-U", "userland_header.bin"])
-  external_apps_start_address,external_apps_end_address = parse_userland_header("userland_header.bin")
-  subprocess.check_output(["rm", "userland_header.bin"])
+  external_apps_flash_start, external_apps_flash_end, external_apps_ram_start, external_apps_ram_end = parse_device_information(device_information)
+
+  # Compute external app address
   external_apps_sector_size = 64 * 1024
   # Flash right slot with external apps
-  download_address = external_apps_start_address + int(app_index) * external_apps_sector_size
-  if download_address >= external_apps_end_address:
+  download_address = external_apps_flash_start + int(app_index) * external_apps_sector_size
+  if download_address >= external_apps_flash_end:
     sys.stderr.write("No more space in external apps range")
     sys.exit(-1)
   print("Download external app at address: " + str(hex(download_address)) + "\n")
@@ -42,6 +41,8 @@ def load_elf(elf_file, app_index = 0):
 
 parser = argparse.ArgumentParser(description="Load ELF file over USB")
 parser.add_argument('elf', metavar='file.elf', help='input ELF file')
+parser.add_argument('--app-index', metavar='i', default=0, help='Specify the application index')
+parser.add_argument('--device-information', default="target/device_information.ld", metavar='file', help='Provide ld file with flash/RAM layout information')
 
 args = parser.parse_args()
-load_elf(args.elf)
+load_elf(args.elf, args.device_information, args.app_index)
