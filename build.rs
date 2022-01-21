f255508bc65be1c8cb048aeec8f47049eb1a43f0
Emilie Feral <emilie.feral@numworks.com>
2022-01-21 12:44:05 +0100

    build.rs: add rule to make target/device_information.ls

--- build.rs
+++ build.rs
@@ -12,4 +12,11 @@ fn main() {
         .args(&["src/icon.png", "target/icon.ld"])
         .output().expect("Failure to launch process");
     assert!(output.status.success(), "{}", String::from_utf8_lossy(&output.stderr));
+
+    // Generate device_information.ld, a linker-embeddable file storing the
+    // targeted device memory layout
+    let output = Command::new("./eadk/get_device_information.py")
+        .args(&["target/device_information.ld"])
+        .output().expect("Failure to launch process");
+    assert!(output.status.success(), "{}", String::from_utf8_lossy(&output.stderr));
 }
