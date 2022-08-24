f19ac6b72e6a49fecbb6543494ccbca76ed5ac20
Romain Goyet <romain.goyet@numworks.com>
2022-08-24 14:07:44 -0400

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@
 
 pub mod eadk;
 
-#[export_name = "eadk_app_name"]
+#[used]
 #[link_section = ".rodata.eadk_app_name"]
 pub static EADK_APP_NAME: [u8; 10] = *b"HelloRust\0";
 
-#[export_name = "eadk_app_api_level"]
-#[link_section = ".rodata.eadk_app_api_level"]
+#[used]
+#[link_section = ".rodata.eadk_api_level"]
 pub static EADK_APP_API_LEVEL: u32 = 0;
 
-#[export_name = "eadk_app_icon"]
+#[used]
 #[link_section = ".rodata.eadk_app_icon"]
 pub static EADK_APP_ICON: [u8; 4250] = *include_bytes!("../target/icon.nwi");
 
@@ -24,7 +24,7 @@ fn random_coordinate() -> u16 {
 }
 
 #[no_mangle]
-fn _eadk_main() {
+pub fn main() {
     for _ in 0..100 {
         let c = eadk::Color { rgb565: random_u16() };
         let r = eadk::Rect { x: random_coordinate(), y: random_coordinate(), width: random_coordinate(), height: random_coordinate() };
